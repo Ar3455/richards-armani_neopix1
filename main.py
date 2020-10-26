454a50939f4159c45325ddf4f79e2adf74b42822
Ar3455 <72777429+Ar3455@users.noreply.github.com>
2020-10-26 13:25:58 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 def on_forever():
     pass
 forever(on_forever)
+light.set_pixel_color(0, light.rgb(255,0,0))
+
+
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
 forever(function on_forever() {
     
 })
+light.setPixelColor(0, light.rgb(255, 0, 0))
